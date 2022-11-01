be2c5205801434b6eb4d68512c0087b4f299250f
Juliette Cordor <grassojames5@gmail.com>
2022-11-01 21:17:42 +1100

    updated sw

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,25 @@
 const cacheName = 'egui-template-pwa';
 const filesToCache = [
+  // Web Root
   './',
+  // HTML Source
   './index.html',
+  // CSS Source
   './styles.css',
+  // JS Source
   './yew-app.js',
+  // WebAssembly Source
   './yew-app_bg.wasm',
+  // All icons
+  'icons/android-chrome-96x96.png',
+  'icons/browserconfig.xml',
+  'icons/favicon-32x32.png',
+  'icons/favicon.ico',
+  'icons/safari-pinned-tab.svg',
+  'icons/apple-touch-icon.png',
+  'icons/favicon-16x16.png',
+  'icons/favicon-384x384.png',
+  'icons/mstile-150x150.png',
 ];
 
 /* Start the service worker and cache all of the app's content */
