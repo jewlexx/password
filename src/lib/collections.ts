68e2bb50df352efe707daadd592227985108ef3a
Juliette Cordor <grassojames5@gmail.com>
2022-11-02 18:55:39 +1100

    updates and fixes

---
+++ src/lib/collections.ts
@@ -0,0 +1,4 @@
+export const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+export const alphabetUpper = alphabet.toUpperCase();
+export const numbers = '0123456789';
+export const symbols = '!@#$%^&*()_+-=[]{};:,./<>?';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,58 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import { alphabet, alphabetUpper, symbols, numbers } from '../lib/collections';
 import styles from '../styles/Home.module.scss';
 
+interface PasswordConfig {
+  length: number;
+  count: number;
+  includeSymbols: boolean;
+  includeNumbers: boolean;
+  includeUppercase: boolean;
+  includeLowerCase: boolean;
+}
+
+export function generatePassword(config: PasswordConfig): string {
+  let totalCollection: string[] = [];
+
+  if (config.includeUppercase) {
+    totalCollection = totalCollection.concat(alphabetUpper.split(''));
+  }
+  if (config.includeLowerCase) {
+    totalCollection = totalCollection.concat(alphabet.split(''));
+  }
+  if (config.includeNumbers) {
+    totalCollection = totalCollection.concat(numbers.split(''));
+  }
+  if (config.includeSymbols) {
+    totalCollection = totalCollection.concat(symbols.split(''));
+  }
+
+  const values = crypto.getRandomValues(new Uint8Array(config.length));
+
+  let password = '';
+
+  for (let i = 0; i < config.length; i++) {
+    const index = values[i] % totalCollection.length;
+    password += totalCollection[index];
+  }
+
+  return password;
+}
+
 export default function Home() {
+  const [config, setConfig] = useState<PasswordConfig>({
+    length: 8,
+    count: 1,
+    includeSymbols: true,
+    includeNumbers: true,
+    includeUppercase: true,
+    includeLowerCase: true,
+  });
+
+  const [password, setPassword] = useState(generatePassword(config));
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,46 +62,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p>{password}</p>
       </main>
 
       <footer className={styles.footer}>
